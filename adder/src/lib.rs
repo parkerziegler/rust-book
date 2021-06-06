b340f46a740d90834176d3c6a0167eb58a47e98f
parkerziegler <parkerellisziegler@gmail.com>
2021-06-06 11:38:02 -0700

    chore: complete section 11.1

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,4 +1,6 @@
-struct Rectangle {
+use std::fmt::format;
+
+pub struct Rectangle {
     width: u32,
     height: u32,
 }
@@ -11,6 +13,24 @@ impl Rectangle {
 
 pub fn add_two(a: i32) -> i32 {
     a + 2
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got: {}", value);
+        }
+
+        Guess { value }
+    }
 }
 
 #[cfg(test)]
@@ -68,6 +88,40 @@ mod tests {
         // The assert_eq! macro compares two values.
         // It's especially useful because it will print
         // both values to stdout if the values are not equal.
+        // Note that this requires the values being compared
+        // to implement the PartialEq and Debug traits.
         assert_eq!(add_two(3), 5);
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Steve");
+        assert!(
+            result.contains("Steve"),
+            "Greeting did not contain name, value was: {}",
+            result
+        )
+    }
+
+    // You can use the [should_panic] attribute after the [test]
+    // attribute to indicate that a test should panic and only pass
+    // if it does indeed panic. You can pass a substring to expected
+    // to indicate the message you expect to be associated with the panic.
+    #[test]
+    #[should_panic(expected = "Guess value must be between 1 and 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    // You can also use Result<T, E> in tests rather than the assert macro.
+    // Note that you cannot use the [should_panic] attribute in a test that
+    // uses Result<T, E> and should instead return the Err type.
+    #[test]
+    fn it_still_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("2 + 2 does not equal 4 😱"))
+        }
     }
 }
