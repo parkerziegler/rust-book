41da91f615aa0c5434eef60af9cdde0488b89ea5
parkerziegler <parkerellisziegler@gmail.com>
2021-06-07 16:15:57 -0700

    chore: complete section 12.2

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     // Collect cmd line arguments using the args function from the std::env module.
@@ -12,4 +13,8 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
